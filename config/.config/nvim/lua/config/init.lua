cd287591a9419877ad8f5289c1f59296c130b618
William Wong <wnineg@gmail.com>
2024-08-05 12:12:07 +0800

    Update the cursor restoration implementation for neovim

--- config/.config/nvim/lua/config/init.lua
+++ config/.config/nvim/lua/config/init.lua
@@ -15,21 +15,23 @@ if vim.env.WSL_DISTRO_NAME then
 end
 
 -- Jump to the last edited position on file open
-vim.api.nvim_create_autocmd('BufReadPost', {
+-- :h restore-cursor
+local restoreCursorGroup = vim.api.nvim_create_augroup('RestoreCursor', { clear = true })
+vim.api.nvim_create_autocmd('BufReadPre', {
     callback = function()
-        local lastPos = vim.api.nvim_buf_get_mark(0, '"')
-        if lastPos == 0 then return end
-        local type = vim.opt.filetype:get()
-        local excludedTypes = { 'gitcommit', 'xxd', 'gitrebase' }
-        local excluded = false
-        for i = 1, #excludedTypes do
-            if type == excludedTypes[i] then
-                excluded = true
-                break
-            end
-        end
-        if excluded then return end
-        vim.api.nvim_win_set_cursor(0, lastPos)
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                local lastPos = vim.api.nvim_buf_get_mark(0, '"')
+                if lastPos == 0 then return end
+                local type = vim.o.filetype
+                local excludedTypes = { 'gitcommit', 'xxd', 'gitrebase' }
+                if vim.tbl_contains(excludedTypes, type) then return end
+                vim.api.nvim_win_set_cursor(0, lastPos)
+            end,
+            buffer = 0,
+            once = true,
+            group = restoreCursorGroup,
+        })
     end,
-    group = vim.api.nvim_create_augroup('vimStartup', { clear = true }),
+    group = restoreCursorGroup,
 })
